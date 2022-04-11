9d37c61ada2d26b778d1975914fbf4a984744a64
Nahnsu <nahnsu@gmail.com>
2022-04-10 22:03:53 -0400

    MAJOR UPDATE TO USE BOOTSTRAP
    Using basic bootstrap nav bar and other functionality. Additionally, a few other more minor updates.

--- javascript.js
+++ javascript.js
@@ -1,69 +1,47 @@
-
-
 function openNav() {
-  document.getElementById("sidebar").style.width = "30%";
-  document.getElementById("main").style.marginLeft = "30%";
-  document.getElementById("main").style.width = "70%";
-  //console.log(x);
+    document.getElementById("sidebar").style.width = "30%";
+    document.getElementById("main").style.marginLeft = "30%";
+    document.getElementById("main").style.width = "70%";
+    //console.log(x);
 }
 
 function closeNav() {
-  document.getElementById("sidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
-  document.getElementById("main").style.width = "100%";
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("main").style.width = "100%";
 }
 
-function fig2(){
-  input = document.querySelector('input[name="choice"]:checked').value;
+function fig2() {
+    input = document.querySelector('input[name="choice"]:checked').value;
 
-  if(input == "none"){
-    document.getElementById("fig2image").src="./resources/unbiased_rock.png";
-  }
+    if (input == "none") {
+        document.getElementById("fig2image").src = "./resources/unbiased_rock.png";
+    }
 
-  if(input == "right"){
-    document.getElementById("fig2image").src="./resources/biased_rock.png";
-  }
+    if (input == "right") {
+        document.getElementById("fig2image").src = "./resources/biased_rock.png";
+    }
 }
 
-function flip(id, text_id, back){
-  var flipcard = document.getElementById(id);
-  var text = document.getElementById(text_id);
-  var temp = document.getElementById('text-holder');
-  
-  flipcard.style.transitionDuration = "2s";
+function flip(id, text_id, back) {
+    var flipcard = document.getElementById(id);
+    var text = document.getElementById(text_id);
+    var temp = document.getElementById('text-holder');
 
-  temp.innerHTML = flipcard.innerHTML;
-  flipcard.innerHTML = text.innerHTML;
-  text.innerHTML = temp.innerHTML;
+    flipcard.style.transitionDuration = "2s";
 
-  if(back==0){
-    flipcard.style.transform = "rotateY(360deg)";
-    flipcard.style.backgroundColor = "lightslategrey";
-    flipcard.style.fontFamily = "'Antic', sans-serif";
-  }
-  if(back==1){
-    flipcard.style.transform = "rotateY(0)";
-    flipcard.style.backgroundColor = "lightsteelblue";
-    flipcard.style.fontFamily = "'Antic Slab', serif";
-  }
-}
+    temp.innerHTML = flipcard.innerHTML;
+    flipcard.innerHTML = text.innerHTML;
+    text.innerHTML = temp.innerHTML;
 
-function comment() {
-  var d = new Date();
-  var date = d.toDateString();
-
-  const newComment = document.createElement("div");
-  const anchor = document.getElementById("comments");
-
-  message = document.commentform.name.value + " says: <br>";
-  message += "<h3>" + document.commentform.text.value + "</h3>";
-  message += "<h6> on " + date + "</h6>";
-
-  newComment.innerHTML = message;
-  newComment.style.backgroundColor = "lightslategrey";
-  newComment.style.margin = "20px";
-  newComment.style.padding = "20px";
-  newComment.style.borderRadius = "20px";
-
-  document.body.appendChild(newComment);
+    if (back == 0) {
+        flipcard.style.transform = "rotateY(360deg)";
+        flipcard.style.backgroundColor = "lightslategrey";
+        flipcard.style.fontFamily = "'Antic', sans-serif";
+    }
+    if (back == 1) {
+        flipcard.style.transform = "rotateY(0)";
+        flipcard.style.backgroundColor = "lightsteelblue";
+        flipcard.style.fontFamily = "'Antic Slab', serif";
+    }
 }
